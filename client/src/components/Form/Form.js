1358ac3f1441931293d49bfff574523d2bba2791
RukshanDias <rukshandias002@gmail.com>
2023-03-08 10:03:46 +0530

    create upto skills select

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,37 +1,150 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { languages, frontend, backend } from "../../data/Skills";
 
 const Form = (props) => {
     const pageNo = props.page;
-    const { watch, register, handleSubmit, formState: { errors } } = useForm({ mode: "all" });
-    const onSubmit = async(data) => {
-        try {
-            const response = await axios.post('/api/form-data', data);
-            console.log(response.data); // response from the server
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    console.log("page " + props.page);
+    const {
+        watch,
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm({ mode: "all" });
+    const socialApps = ["twitter", "stackoverflow", "facebook", "linkedin", "youtube", "Medium", "discord"];
+
+    const displaySkills = (object) => {
+        return (
+            <div className="flex flex-wrap">
+                {object.map((element, index) => (
+                    <div key={index} className="flex items-center w-1/4 my-1">
+                        <input type="checkbox" className="mr-1 cursor-pointer" id={element.name} name={element.name} value={element.link} />
+                        <label htmlFor={element.name} className="flex items-center cursor-pointer">
+                            <img src={element.link} alt={element.name} className="w-10 h-10 mr-1" />
+                        </label>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+    const onSubmit = (data, e) => {
+        console.log(data, e);
+        sendFormData(data);
+    };
+
+    const sendFormData = (data) => {
+        axios
+            .post("http://localhost:8000/api", data)
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
+                {/* ABOUT YOU */}
                 {pageNo === 0 && (
                     <div>
-                        <input type="url" placeholder="Github Profile Link" {...register("Github Profile Link", { required: true, maxLength: 80 })} /><br />
-                        <input type="text" placeholder="Name" {...register("Name", { required: true, maxLength: 100 })} /><br />
-                        <label htmlFor='username' />
-                        <input type="text" id='username' {...register("username", { required: true, value: 'error' })} />
+                        <label htmlFor="githubLink" className="inline-block mt-3 mb-1.5">
+                            Github Username
+                        </label>
+                        <input
+                            className="w-full bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg outline-blue-600 p-2.5"
+                            type="text"
+                            id="githubLink"
+                            placeholder="Github User Name"
+                            {...register("GithubLink", {
+                                required: true,
+                            })}
+                        />
+                        {errors.GithubLink && <p className="form-error-msg">This field is required</p>}
+
+                        <label htmlFor="name" className="inline-block mt-3 mb-1.5">
+                            Name
+                        </label>
+                        <input
+                            className=" w-full bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg outline-blue-600 p-2.5"
+                            type="text"
+                            id="name"
+                            placeholder="Name"
+                            {...register("Name", { required: true, maxLength: 100 })}
+                        />
+                        {errors.Name && <p className="form-error-msg">This field is required</p>}
+
+                        <label htmlFor="personalStatement" className="inline-block mt-3 mb-1.5">
+                            Personal Statement
+                        </label>
+                        <input
+                            className="w-full bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg outline-blue-600 p-2.5"
+                            type="text"
+                            id="personalStatement"
+                            {...register("personalStatement", { required: true })}
+                        />
+
+                        <label htmlFor="work" className="inline-block mt-3 mb-1.5">
+                            Work
+                        </label>
+                        <textarea
+                            className="w-full bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg outline-blue-600 p-2.5"
+                            rows={5}
+                            id="work"
+                            defaultValue={`🔭 I’m currently working on: ...\n👯 I’m looking to collaborate on: ...\n💬 Ask me about: ...\n⚡ Fun fact: ...`}
+                            {...register("work")}
+                        />
                     </div>
                 )}
 
-                <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} /><br />
-                <input type="checkbox" placeholder="Technologies" {...register("Technologies", { required: true, maxLength: 12 })} />
+                {/* SKILLS */}
+                {pageNo === 1 && (
+                    <>
+                        <h2 className="mb-3">Languages</h2>
+                        {displaySkills(languages)}
+                        <h2 className="my-3">Frontend</h2>
+                        {displaySkills(frontend)}
+                        <h2 className="my-3">Backend</h2>
+                        {displaySkills(backend)}
+                    </>
+                )}
+
+                {/* LINKS */}
+                {pageNo === 2 && (
+                    <div>
+                        <input
+                            type="url"
+                            placeholder="Github Profile Link"
+                            {...register("GithubProfileLink", {
+                                required: true,
+                                maxLength: 80,
+                            })}
+                        />
+                        {errors.GithubProfileLink && <p>This field is required</p>}
+                        <br />
+                    </div>
+                )}
+
+                {/* OTHERS */}
+                {pageNo === 3 && (
+                    <div>
+                        <input
+                            type="url"
+                            placeholder="Github Profile Link"
+                            {...register("GithubProfileLink", {
+                                required: true,
+                                maxLength: 80,
+                            })}
+                        />
+                        {errors.GithubProfileLink && <p>This field is required</p>}
+                        <br />
+                        <button type="submit">Generate</button>
+                    </div>
+                )}
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Form
+export default Form;
--- client/src/components/Form/FormCard.js
+++ client/src/components/Form/FormCard.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import { useState } from 'react';
-import Form from './Form';
+import React from "react";
+import { useState, useRef } from "react";
+import Form from "./Form";
 
 const FormCard = () => {
-    const [page, setpage] = useState(0);
-    const formHeaders = ["Account Detals", "About You", "Social media", "Technologies"];
-    // const FormDisplay = () => {
-    //     switch (page) {
-    //         case 0:
-    //             return <AccountForm />
-    //         case 1:
-    //             return <AboutMeForm />
-    //         default:
-    //             <h2>error</h2>
-    //     }
-    // }
+  const [page, setpage] = useState(0);
+  const formHeaders = ["About You", "Skills", "Links", "Others"];
+  const nextBtnRef = useRef(null);
+  const childRef = useRef();
 
-    return (
-        <div className='Form bg-neutral-100 px-10 py-12 rounded-md'>
-            <div className='progressbar'>
-                <p>Step {page} of {formHeaders.length}</p>
-            </div>
-
-            <div className='form-container'>
-                <div className='header text-center'>
-                    <h2>{formHeaders[page]}</h2>
-                </div>
-                <div className='form-body'>
-                    <Form page={page}/>
-                </div>
-                <div className='footer flex justify-around mt-6'>
-                    <button
-                        className='bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer'
-                        disabled={page <= 0}
-                        onClick={() => setpage(() => page - 1)}
-                    >Back</button>
-                    <button
-                        className='bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer'
-                        disabled={page === formHeaders.length - 1}
-                        onClick={() => setpage(() => page + 1)}
-                    >Next</button>
-                </div>
-            </div>
+  return (
+    <div className="Form bg-neutral-100 px-10 py-12 rounded-md w-1/3 my-2">
+      <div className="form-container">
+        <div className="progressbar text-gray-500 text-sm font-semibold">
+          <p>
+            Step {page + 1} of {formHeaders.length}
+          </p>
         </div>
-    )
-}
 
-export default FormCard
+        <div className="header text-center mt-2 mb-6 text-4xl font-mono text-purple-700">
+          <h2>{formHeaders[page]}</h2>
+        </div>
+
+        <div className="form-body overflow-auto">
+          <Form page={page} />
+        </div>
+
+        <div className="footer flex justify-around mt-5">
+          <button
+            className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer"
+            disabled={page <= 0}
+            onClick={() => setpage(() => page - 1)}
+          >
+            Back
+          </button>
+
+          {/* redering Next btn or Submit btn */}
+          {page === formHeaders.length - 1 ? (
+            <button type="submit">Submit</button>
+          ) : (
+            <button
+              className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer"
+              ref={nextBtnRef}
+              disabled={page === formHeaders.length - 1}
+              onClick={() => setpage(() => page + 1)}
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormCard;
