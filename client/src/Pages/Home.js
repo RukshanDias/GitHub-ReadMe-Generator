ce49cf59cf3d8e58fc75611c611497939c9691d7
RukshanDias <rukshandias002@gmail.com>
2023-03-09 14:38:24 +0530

    create Home page

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormCard from "../components/Form/FormCard";
+
+const Home = () => {
+    return (
+        <div className="App flex justify-center items-center min-h-screen">
+            <FormCard />
+        </div>
+    );
+};
+
+export default Home;
