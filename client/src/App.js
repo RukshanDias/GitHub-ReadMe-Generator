a0cc49819f6cdb4342f6ba6acfeb58ddb3dc4b44
RukshanDias <rukshandias002@gmail.com>
2023-03-09 14:36:25 +0530

    add context

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import FormCard from './components/Form/FormCard'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Generate from "./Pages/Generate";
+import MarkdownContext from "./context/MarkdownContext";
 
 const App = () => {
-  return (
-    <div className='App flex justify-center items-center min-h-screen'>
-      <FormCard />
-    </div>
-  )
-}
+    const [responseData, setResponseData] = useState(null);
+    return (
+        <MarkdownContext.Provider value={{ responseData, setResponseData }}>
+            <BrowserRouter>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/generate" element={<Generate />} />
+                </Routes>
+            </BrowserRouter>
+        </MarkdownContext.Provider>
+    );
+};
 
-export default App
+export default App;
