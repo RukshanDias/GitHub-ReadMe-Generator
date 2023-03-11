218d2cac5808db07c730b05d0f52d445288480b1
RukshanDias <rukshandias002@gmail.com>
2023-03-11 17:33:24 +0530

    the basic server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,22 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const generateMarkdown = require("./generateMarkdown");
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/api", (req, res) => {
+    const formData = req.body;
+    console.log(formData);
+    // res.send("Form submitted successfully!");
+    const markdown = generateMarkdown(formData);
+    res.send(markdown);
+});
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+});
