74d689169d7b27f9d68a98f10d37ebb591bc34b4
RukshanDias <rukshandias002@gmail.com>
2023-03-11 17:31:17 +0530

    finalized editor component

--- client/src/components/Generate/Editor.js
+++ client/src/components/Generate/Editor.js
@@ -9,15 +9,20 @@ const Editor = (props) => {
         }
     };
     return (
-        <div className="w-1/2 px-16 py-5 bg-slate-100 mx-5 my-5">
-            <textarea onChange={props.handlechange} className="editor w-full" id="finalMarkdown">
+        <div className="w-2/5 max-h-screen px-16 py-5 bg-neutral-100 mx-5 my-5 overflow-auto">
+            <textarea
+                onChange={props.handlechange}
+                className="w-full p-3 border-2 border-gray-900 rounded-md bg-neutral-100"
+                id="finalMarkdown"
+                rows={22}
+            >
                 {props.text}
             </textarea>
             <div className="flex justify-around">
-                <button className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer" onClick={handleCopy}>
+                <button className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer mt-2" onClick={handleCopy}>
                     Copy
                 </button>
-                <button className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer">Download</button>
+                <button className="bg-purple-800 text-stone-50 px-4 py-1 rounded-lg cursor-pointer mt-2">Download</button>
             </div>
         </div>
     );
