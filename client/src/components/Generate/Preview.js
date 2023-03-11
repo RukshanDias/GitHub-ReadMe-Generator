942dfc3d6c40e576e25c727b14e5fe6aa19d2ad1
RukshanDias <rukshandias002@gmail.com>
2023-03-11 17:31:49 +0530

    finalized preview component

--- client/src/components/Generate/Preview.js
+++ client/src/components/Generate/Preview.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
 const Preview = (props) => {
     return (
-        <div className="w-1/2 px-16 py-5 bg-slate-100 mx-5 my-5 overflow-auto">
-            <ReactMarkdown className="markdown">{props.text}</ReactMarkdown>;
+        <div className="w-2/5 max-h-screen px-8 py-5 bg-neutral-100 mx-5 my-5 overflow-auto" style={{maxHeight:"88vh"}}>
+            <ReactMarkdown className="markdown" rehypePlugins={[rehypeRaw]}>
+                {props.text}
+            </ReactMarkdown>
+            ;
         </div>
     );
 };
