9595b25920d0ca87f0d5ae2289ffa5cb79882b52
RukshanDias <rukshandias002@gmail.com>
2023-03-12 10:54:17 +0530

    add svg to home

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import FormCard from "../components/Form/FormCard";
+import YourSvg from "../assets/coding.svg";
 
 const Home = () => {
     return (
-        <div className="App flex justify-center items-center min-h-screen">
+        <div className="App flex justify-center items-center min-h-screen flex-row">
+            <div className="img-container w-1/3 relative mt-30 mr-24">
+                <div className="backdrop"></div>
+                <img src={YourSvg} alt="Your SVG" className="" />
+            </div>
             <FormCard />
         </div>
     );
