737eac7d41eb943d33e3d19e277e3aaf715b37bf
RukshanDias <rukshandias002@gmail.com>
2023-03-14 06:52:29 +0530

    update server url

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.post("/api", (req, res) => {
+app.post(process.env.SERVER_URL, (req, res) => {
     const formData = req.body;
     console.log(formData);
     // res.send("Form submitted successfully!");
